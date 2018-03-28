9395d77ffba128867bfaeffd1e98154b57c17ddf
sresh12 <srajesh@wpi.edu>
2018-03-28 09:19:43 -0400

    Model receipt

--- app/src/main/java/com/example/deepa/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/deepa/inventoryapp/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.deepa.inventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void readReceipt() {
+        InputStream is = getResources().openRawResource(R.raw.modelReceipt);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line = "";
+        ArrayList<Item> newReceipt = new ArrayList<>();
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                newReceipt.add(new Item(line));
+            }
+        }
+        catch(IOException e) {
+            Log.wtf("ImageData", "ya dun messed up");
+        }
     }
 }
