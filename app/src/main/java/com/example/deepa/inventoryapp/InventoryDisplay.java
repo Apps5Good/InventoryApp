2f383e7a17e7f272382fa4689ed2c41a20360075
ryanravitz <rjravitz@wpi.edu>
2018-04-06 12:54:16 -0400

    knock knock this is listview

--- app/src/main/java/com/example/deepa/inventoryapp/InventoryDisplay.java
+++ app/src/main/java/com/example/deepa/inventoryapp/InventoryDisplay.java
@@ -2,6 +2,10 @@ package com.example.deepa.inventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class InventoryDisplay extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class InventoryDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory_display);
+
+        String[] foods = {"gefilte fish", "jelly", "glabgogabgalab", "brownies", "kugel"};
+        ListAdapter myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        EditText inputSearch;
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+        //inputSearch = (EditText) findViewById(R.id.searchBar);
+        myListView.setAdapter(myAdapter);
+
+
     }
 }
+
+
