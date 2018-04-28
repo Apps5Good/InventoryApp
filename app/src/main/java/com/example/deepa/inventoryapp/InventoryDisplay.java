3fcd2316baddf6feca844e83c87eedf126eb310a
ryanravitz <rjravitz@wpi.edu>
2018-04-28 15:37:38 -0400

    itemclick

--- app/src/main/java/com/example/deepa/inventoryapp/InventoryDisplay.java
+++ app/src/main/java/com/example/deepa/inventoryapp/InventoryDisplay.java
@@ -19,6 +19,10 @@ import java.util.List;
 public class InventoryDisplay extends AppCompatActivity {
 
     ArrayList<Item> items;
+    ListView myListView;
+    String ItemName;
+    int ItemQuantity;
+    Item item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +34,32 @@ public class InventoryDisplay extends AppCompatActivity {
 
         List<Item> items = db.userDao().getAllItems();
 
-        ListAdapter myAdapter = new ArrayAdapter<Item>(this, android.R.layout.simple_list_item_1, items);
-        //EditText inputSearch;
+       ArrayAdapter<Item> myAdapter = new ArrayAdapter<Item>(this, android.R.layout.simple_list_item_1, items);
 
-        ListView myListView = (ListView) findViewById(R.id.myListView);
+
+        final ListView myListView = (ListView) findViewById(R.id.myListView);
 
         myListView.setAdapter(myAdapter);
-       /* myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                   Intent intentItemInfo = new Intent(InventoryDisplay.this, ItemInfo.class);
-                   Item selectedItem = parent<Item>[position];
 
-                   startActivity(intentItemInfo);
+
+
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l){
+            Intent intent = new Intent(InventoryDisplay.this, ItemInfo.class);
+            item = (Item) myListView.getItemAtPosition(i);
+            ItemName = item.getItemName();
+            ItemQuantity = item.getItemQuantity();
+
+            intent.putExtra("ItemName", ItemName);
+            intent.putExtra("ItemQuantity",ItemQuantity);
+
+            startActivity(intent);
+
             }
-        });*/
-
+        });
+         myListView.setAdapter(myAdapter);
     }
 
     public void toHome(View v) {
@@ -58,6 +73,14 @@ public class InventoryDisplay extends AppCompatActivity {
 
         startActivity(intentCreate);
     }
+
+    public void toRemoveItem (View v){
+        Intent intentRemove = new Intent (this, ListPreview.class);
+        startActivity(intentRemove);
+    }
+
+
 }
+
 
 
